5d332b5ae852c6e257ce79c9a7bdda54e36cb160
Rajat Mathur <mathurrajat35@gmail.com>
2025-07-28 10:17:10 +0530

    Add files via upload

---
+++ Rajat Coding Files/Video 55/script.js
@@ -0,0 +1,43 @@
+console.log("hey i am good")
+
+let a = 55
+
+let b = 50 
+let c = "Hey This Is Part 55 of Sigma Web Development Course"
+let d = true
+let e = 2.55
+let af = 3
+
+
+{
+    let a =66
+    console.log(a)
+}
+
+console.log(a)
+
+let w =3
+let x =78
+let y ="hey i am good"
+let z =true
+const p = undefined
+let q = null
+
+console.log(w,x,y,z,p,q)
+console.log(typeof w,typeof x,typeof y,typeof z,typeof p,typeof q)
+
+
+let o = {
+     jobtitle:"software developer",
+    name:"My name is rajat "
+   
+}
+
+console.log(o)
+
+o.salary = "500crores"
+console.log(o)
+
+o.place = "gwalior"
+
+console.log(o)

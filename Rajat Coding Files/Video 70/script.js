ff3c153fbe2a2025acc2beefc8bd0868a9f29ab9
Rajat Mathur <mathurrajat35@gmail.com>
2025-10-02 13:30:46 +0530

    Add files via upload

---
+++ Rajat Coding Files/Video 70/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+let Rajat = document.getElementsByClassName("box")
+console.log(Rajat)
+Rajat[0].style.backgroundColor ="orange"
+Rajat[0].style.color = "red"
+
+Rajat[1].style.backgroundColor ="pink"
+Rajat[1].style.color = "green"
+
+Rajat[2].style.backgroundColor ="green"
+Rajat[2].style.color = "skyblue"
+
+Rajat[3].style.backgroundColor ="skyblue"
+Rajat[3].style.color = "violet"
+
+Rajat[4].style.backgroundColor ="darkorange"
+Rajat[4].style.color = "#a909eeff"

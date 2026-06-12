bc976f4809dc9897f0d97252d22aa81eed02f2dd
Rajat Mathur <mathurrajat35@gmail.com>
2026-06-12 22:33:51 +0530

    Add files via upload

---
+++ Rajat Coding Files/Video 74/script.js
@@ -0,0 +1,18 @@
+// let button = document.getElementById("btn")
+// button.addEventListener("dblclick",()=>{
+//     document.querySelector(".box").innerHTML = "<b>Hey we will throw a party because we were clicked</b> Enjoy Bro"
+// })
+
+
+
+// let button = document.getElementById("btn")
+// button.addEventListener("click",()=>{
+//     alert("Hey we were clicked")
+// })
+
+
+
+let button = document.getElementById("btn")
+button.addEventListener("contentmenu",(e)=>{
+    alert("dont hacked by right click")
+})
